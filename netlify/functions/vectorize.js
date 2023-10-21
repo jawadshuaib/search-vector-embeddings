f7aaf56e818f4d4d02454fecaee430bc8ce91334
Jawad <jawad.php@gmail.com>
2023-10-21 02:54:14 -0400

    Added lambda fns

---
+++ netlify/functions/vectorize.js
@@ -0,0 +1,23 @@
+/* eslint-env node */
+
+export async function handler(event) {
+  // if (event.httpMethod !== 'POST')
+  //   return { statusCode: 405, body: 'Method Not Allowed' };
+
+  // const token = process.env.OPENAI_API_KEY;
+  // const { model, payload, temperature } = JSON.parse(event.body);
+
+  // const { context, input } = payload;
+
+  // let headers = {};
+
+  // headers = {
+  //   'Content-Type': 'application/json',
+  //   Authorization: `Bearer ${token}`,
+  // };
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Hello from Search' }),
+  };
+}
