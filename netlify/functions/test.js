b458fbcb55795998f46d99bb24832e6628b39f72
Jawad <jawad.php@gmail.com>
2023-10-21 11:36:45 -0400

    added test fn

---
+++ netlify/functions/test.js
@@ -0,0 +1,32 @@
+/* eslint-env node */
+// https://search-vector-embeddings.netlify.app/.netlify/functions/vectorize
+export async function handler(event) {
+  if (event.httpMethod !== 'POST')
+    return { statusCode: 405, body: 'Method Not Allowed' };
+
+  const token = process.env.OPENAI_API_KEY;
+  const { model, input } = JSON.parse(event.body);
+
+  try {
+    const response = await fetch('https://api.openai.com/v1/embeddings', {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      method: 'POST',
+      body: JSON.stringify({ input, model }),
+    });
+
+    const { data } = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ embedding: data[0].embedding }),
+    };
+  } catch (error) {
+    return {
+      statusCode: error.response ? error.response.status : 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+}
--- src/services/getVector.js
+++ src/services/getVector.js
@@ -3,7 +3,7 @@ import { siteInfo } from '../utils/settings';
 export default async function getVector(input) {
   const model = 'text-embedding-ada-002';
   try {
-    const response = await fetch(`${siteInfo.fnPath}/vectorize`, {
+    const response = await fetch(`${siteInfo.fnPath}/test`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
